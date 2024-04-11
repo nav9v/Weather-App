6b9ca004bc35811d9cc81704861189c8e6b3ea65
navneet <navneetsharma2302@gmail.com>
2024-04-11 10:49:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Weather App
+
+This is a simple weather application that uses the OpenWeatherMap API to fetch and display weather data for a specific city.
+
+## Features
+
+- Fetches and displays the following weather data:
+  - City name
+  - Temperature
+  - Humidity
+  - Cloudiness
+
+## Deployment
+
+The live version of the application can be accessed at the following link:
+
+[Check Weather ☁️🌡️❄️](https://coruscating-beijinho-bd4f72.netlify.app/)
+
+## Local Setup
+
+1. Clone the repository to your local machine.
+2. Open the `index.html` file in your browser.
+
+## How to Use
+
+1. Enter the name of the city in the text area.
+2. Click the "WEATHER" button to fetch and display the weather data.
+
+## API Used
+
+This application uses the [OpenWeatherMap API](https://openweathermap.org/).
---
+++ script.js
@@ -0,0 +1,40 @@
+import apiKey from "./data.js"
+
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const humid = document.querySelector(".humid");
+const cloud = document.querySelector(".cloud");
+
+document.getElementById('textarea').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        var cityName = event.target.value;
+        getWeather(cityName);
+    }
+});
+const button = document.querySelector(".pixel-grid");
+
+button.addEventListener('click', () => {
+    const cityName = document.querySelector(".txtarea").value;
+    getWeather(cityName);
+});
+
+async function getWeather(cityName) {
+    if (!cityName.trim()) {
+        alert('Please enter a city name :(');
+        city.textContent = 'Please enter a city name :(';
+        return;
+    }
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
+    const data = await response.json();
+    console.log(data);
+    updateUI(data);
+
+}
+
+function updateUI(data) {
+    city.textContent = data.name;
+    temp.textContent = "Temp: " + Math.round(data.main.temp) + "°C";
+    humid.textContent = "humidity: " + Math.round(data.main.humidity);
+    cloud.textContent = "Sky: " + data.weather[0].description;
+}
